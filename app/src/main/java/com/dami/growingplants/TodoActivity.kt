5059401ff92333893cbd0782b64250e1a2e6fcd1
dami <dami0804@naver.com>
2022-11-21 21:21:50 +0900

    todoList

--- app/src/main/java/com/dami/growingplants/TodoActivity.kt
+++ app/src/main/java/com/dami/growingplants/TodoActivity.kt
@@ -3,7 +3,10 @@ package com.dami.growingplants
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.SparseBooleanArray
+import android.widget.ArrayAdapter
 import android.widget.ImageView
+import kotlinx.android.synthetic.main.activity_todo.*
 
 class TodoActivity : AppCompatActivity() {
     lateinit var calBtn: ImageView
@@ -12,6 +15,48 @@ class TodoActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_todo)
+
+            var itemlist = arrayListOf<String>()
+            var adapter = ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_multiple_choice
+                , itemlist)
+
+
+
+            // add버튼 클릭
+            add.setOnClickListener {
+                itemlist.add(editText.text.toString())
+                listView.adapter =  adapter
+                adapter.notifyDataSetChanged()
+                // This is because every time when you add the item the input space or the eidt text space will be cleared
+                editText.text.clear()
+            }
+            // Clear버튼 클릭
+            clear.setOnClickListener {
+
+                itemlist.clear()
+                adapter.notifyDataSetChanged()
+            }
+            //
+            listView.setOnItemClickListener { adapterView, view, i, l ->
+                android.widget.Toast.makeText(this, "You Selected the item --> "+itemlist.get(i), android.widget.Toast.LENGTH_SHORT).show()
+            }
+            // 삭제버튼 클릭
+            delete.setOnClickListener {
+                val position: SparseBooleanArray = listView.checkedItemPositions
+                val count = listView.count
+                var item = count - 1
+                while (item >= 0) {
+                    if (position.get(item))
+                    {
+                        adapter.remove(itemlist.get(item))
+                    }
+                    item--
+                }
+                position.clear()
+                adapter.notifyDataSetChanged()
+            }
+
 
         calBtn = findViewById(R.id.CalBtn)
         toDoBtn = findViewById(R.id.TodoBtn)
