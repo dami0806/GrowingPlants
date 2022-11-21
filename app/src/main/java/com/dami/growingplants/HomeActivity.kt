2db2826f13c0e9099da32af29b1ff2087bc8a62e
dami <dami0804@naver.com>
2022-11-21 21:11:23 +0900

    달력

--- app/src/main/java/com/dami/growingplants/HomeActivity.kt
+++ app/src/main/java/com/dami/growingplants/HomeActivity.kt
@@ -4,10 +4,17 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ImageView
+import android.widget.CalendarView
+import android.widget.CalendarView.OnDateChangeListener
+import android.widget.TextView
+
 
 class HomeActivity : AppCompatActivity() {
     lateinit var calBtn:ImageView
     lateinit var toDoBtn:ImageView
+    lateinit var dateTV: TextView
+    lateinit var calendarView: CalendarView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
@@ -15,7 +22,25 @@ class HomeActivity : AppCompatActivity() {
         calBtn = findViewById(R.id.CalBtn)
         toDoBtn = findViewById(R.id.TodoBtn)
 
-        //
+
+                dateTV = findViewById(R.id.idTVDate)
+                calendarView = findViewById(R.id.calendarView)
+
+
+                calendarView
+                    .setOnDateChangeListener(
+                        OnDateChangeListener { view, year, month, dayOfMonth ->
+
+                            val Date = (dayOfMonth.toString() + "-"
+                                    + (month + 1) + "-" + year)
+
+                            // set this date in TextView for Display
+                            dateTV.setText(Date)
+                        })
+
+
+
+        //Tap
         calBtn.setOnClickListener {
 
         }
