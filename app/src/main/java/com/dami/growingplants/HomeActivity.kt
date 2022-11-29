49d98925f9d8698d6dca1965ce8170b453984021
dami <dami0804@naver.com>
2022-11-29 13:47:00 +0900

    알림전까지 완료

--- app/src/main/java/com/dami/growingplants/HomeActivity.kt
+++ app/src/main/java/com/dami/growingplants/HomeActivity.kt
@@ -8,6 +8,7 @@ import android.util.Log
 import android.widget.*
 import android.widget.CalendarView.OnDateChangeListener
 import androidx.annotation.RequiresApi
+import androidx.appcompat.app.AlertDialog
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.ValueEventListener
@@ -25,19 +26,16 @@ class HomeActivity : AppCompatActivity() {
     lateinit var dateTV: TextView
     lateinit var calendarView: CalendarView
     private lateinit var key: String
-    private lateinit var keyL: String
     private lateinit var fbkey: String
-    private lateinit var  keylist: ArrayList<String>
     private lateinit var Datetext: String
     private lateinit var listviewAdapter: ListViewAdapter4
     val list_itemText = arrayListOf<String>()
     var keyList = ArrayList<String>()
-    var d:String?=null
     val list_item = mutableListOf<ListViewModel>()
     val list_itemtext = mutableListOf<String>()
     var textList = arrayListOf<String>()
     lateinit var add: ImageView
-
+    lateinit var clear: ImageView
     @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -47,7 +45,11 @@ class HomeActivity : AppCompatActivity() {
         toDoBtn = findViewById(R.id.TodoBtn)
         dateTV = findViewById(R.id.idTVDate)
         calendarView = findViewById(R.id.calendarView)
-add = findViewById(R.id.add)
+        add = findViewById(R.id.add)
+        clear = findViewById(R.id.clear)
+        clear.setOnClickListener {
+            clearBtn()
+        }
         calendarView.setOnDateChangeListener(
             OnDateChangeListener { view, year, month, dayOfMonth ->
 
@@ -56,11 +58,10 @@ add = findViewById(R.id.add)
 
                 // set this date in TextView for Display
                 dateTV.setText(Datetext)
-                Log.d("테텥ㄱ",getDateData(dateTV.text.toString()).toString()) //글의 날짜정보 가져옴
+
                 getCommentData(dateTV.text.toString())
 
             })
-
 
         val listview = findViewById<ListView>(R.id.listView)
         listviewAdapter = ListViewAdapter4(list_item)
@@ -68,18 +69,12 @@ add = findViewById(R.id.add)
 
        add.setOnClickListener {
         addTask()}
-        clear.setOnClickListener {
-        clearBtn()}
-
-        listview.setOnItemClickListener { parent, view, position, id ->
 
 
-
+        listview.setOnItemLongClickListener { parent, view, position, id ->
+           removeList(position)
+            true
         }
-
-
-
-
 
 
         toDoBtn.setOnClickListener {
@@ -97,13 +92,68 @@ add = findViewById(R.id.add)
 
 
     }
+    private fun removeList(position: Int) {
+        var dlg = AlertDialog.Builder(this)
+        dlg.setTitle("growingPlant")
+        dlg.setMessage("이 일정을 삭제하시겠습니까?")
+        dlg.setIcon(R.drawable.img_2)
+
+        dlg.setNegativeButton("삭제") { dialog, which ->
+
+
+            val postListener = object : ValueEventListener {
+                override fun onDataChange(dataSnapshot: DataSnapshot) {
+                    for (dataModel in dataSnapshot.children) { //datModel: key값
+
+                        var selectitem=textList[position]
+                        Log.d("selectitem",selectitem.toString())
+                        Log.d("selectitem",dataModel.getValue(ListViewModel::class.java)!!.text.toString())
+                        if (selectitem == dataModel.getValue(ListViewModel::class.java)!!.text.toString()) //클릭한 list[postiion]time과 dateModel의 value time 같을시 삭제
+                        {
+                            Log.d("selectitem1",dataModel.key.toString())
+                            UserApiClient.instance.me { user, error ->
+                                FBRef.todoDate
+                                    .child(user!!.id.toString())
+                                    .child(dateTV.text.toString())
+                                    .child(dataModel.key.toString())
+                                    .removeValue()
+                            }
+
+                        }
+                    }
+                }
+
+                override fun onCancelled(databaseError: DatabaseError) {
+                    // Getting Post failed, log a message
+                    Log.w(
+                        "ContentListActivity",
+                        "loadPost:onCancelled",
+                        databaseError.toException()
+                    )
+                }
+            }
+
+            UserApiClient.instance.me { user, error ->
+                fbkey = FBRef.todoDate.push().key.toString() //이미지이름에 쓰려고 먼저 키값 받아옴
+                FBRef.todoDate
+                    .child(user!!.id.toString())
+                    .child(dateTV.text.toString())
+                    .addValueEventListener(postListener)
+
+            }
+
+            Toast.makeText(this, "삭제되었습니다.", Toast.LENGTH_SHORT).show()
+
+        }
+        dlg.setPositiveButton("취소", null)
+        dlg.show()
+
+    }
 fun addTask(){
 
        list_itemText.clear()
          Log.d("비교", dateTV.text.toString())
          list_item.add(ListViewModel(editText.text.toString(),"false"))
-
-
 
          var et = editText.text.toString()
        Log.d("보기",et.toString())
@@ -121,42 +171,44 @@ fun addTask(){
          editText.text.clear()
 
 }
-    fun deleteTask(){
 
-        list_itemText.clear()
-
-        //FB에 넣기
-        UserApiClient.instance.me { user, error ->
-            fbkey = FBRef.todoDate.push().key.toString() //이미지이름에 쓰려고 먼저 키값 받아옴
-            FBRef.todoDate
-                .child(user!!.id.toString())
-                .child(dateTV.text.toString())
-                .removeValue()
-
-        }
-
-
-
-    }
     fun clearBtn(){
-        list_item.clear()
-        listviewAdapter.notifyDataSetChanged()
-        UserApiClient.instance.me { user, error ->
+        var dlg = AlertDialog.Builder(this)
+        dlg.setTitle("growingPlant")
+        dlg.setMessage("전체 일정을 삭제하시겠습니까?")
+        dlg.setIcon(R.drawable.img_2)
 
-            FBRef.todoDate
-                .child(user!!.id.toString())
-                .child(dateTV.text.toString())
-                .removeValue()
+        dlg.setNegativeButton("삭제") { dialog, which ->
+
+
+            list_item.clear()
+            listviewAdapter.notifyDataSetChanged()
+            UserApiClient.instance.me { user, error ->
+
+                FBRef.todoDate
+                    .child(user!!.id.toString())
+                    .child(dateTV.text.toString())
+                    .removeValue()
+            }
+            getCommentData(dateTV.text.toString())
+            listviewAdapter.notifyDataSetChanged()//어댑터 동기화
+
+            Toast.makeText(this, "초기화 되었습니다.", Toast.LENGTH_SHORT).show()
+
         }
-        getCommentData(dateTV.text.toString())
-        listviewAdapter.notifyDataSetChanged()//어댑터 동기화
+        dlg.setPositiveButton("취소", null)
+        dlg.show()
+
     }
+
+
 fun getCommentData(date: String) {
 //comment 아래 board아래 commentkey 아래 comment 데이터들
   val postListener = object : ValueEventListener {
       override fun onDataChange(dataSnapshot: DataSnapshot) {
           list_item.clear()
           list_itemtext.clear()
+          textList.clear()
           for (dataModel in dataSnapshot.children) {
               if (date == dataModel.key) {
 
@@ -166,6 +218,7 @@ fun getCommentData(date: String) {
                       //val item = i.getValue(ListViewModel::class.java)
                       //  Log.d("확인인",i.getValue(ListViewModel::class.java).toString())
                       list_item.add(i.getValue(ListViewModel::class.java)!!)
+                      textList.add(i.getValue(ListViewModel::class.java)!!.text.toString())
                       keyList.add(i.key.toString())
                       for (j in i.children) {
 
@@ -189,62 +242,5 @@ fun getCommentData(date: String) {
   }
 }
 
-fun getDateData(dateData:String): String? {
-
-//comment 아래 board아래 commentkey 아래 comment 데이터들
-  val postListener = object : ValueEventListener {
-      override fun onDataChange(dataSnapshot: DataSnapshot) {
-
-          for (dataModel in dataSnapshot.children) {
-             d =  dataModel.key.toString()
-              if(d==dateData){
-              Log.d("테텥ㄱ1",d.toString()) //글의 날짜정보 가져옴
-
-                  for(i in dataModel.children){
-
-                  }
-          }}
-      }
-
-      override fun onCancelled(databaseError: DatabaseError) {
-      }
-  }
-  UserApiClient.instance.me { user, error ->
-      FBRef.todoDate.child(user!!.id.toString())
-          .addValueEventListener(postListener) }
-return d
-}
-    fun TextData(dateTV:String) {
-        val postListener = object : ValueEventListener {
-            override fun onDataChange(dataSnapshot: DataSnapshot) {
-
-                for (dataModel in dataSnapshot.children) {
-
-
-                    for (i in dataModel.children) {
-                        //Log.d("담담",i.value.toString())
-                        Log.d("담담", i.toString())
-                        if (i.key == "text") {
-                            Log.d("담담1", i.value.toString())
-                            textList.add(i.value.toString())
-
-                        }
-                    }
-                    Log.d("담담2", textList.toString())
-                }
-
-            }
-
-            override fun onCancelled(databaseError: DatabaseError) {
-            }
-        }
-        UserApiClient.instance.me { user, error ->
-            FBRef.todoDate
-                .child(user!!.id.toString())
-                .child(dateTV)
-                .addValueEventListener(postListener)
-        }
-
-    }
 
 }
--- app/src/main/java/com/dami/growingplants/TodoActivity.kt
+++ app/src/main/java/com/dami/growingplants/TodoActivity.kt
@@ -1,6 +1,7 @@
 package com.dami.growingplants
 
 import android.content.Intent
+import android.icu.text.Transliterator
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -25,19 +26,14 @@ class TodoActivity : AppCompatActivity() {
     lateinit var toDoBtn: ImageView
     lateinit var editText: EditText
     private lateinit var listviewAdapter: ListViewAdapter4
-
     lateinit var add: ImageView
-    lateinit var clear: Button
-    lateinit var delete: Button
-    var d:String?=null
+    var dateTV:String = ""
     val list_item = mutableListOf<ListViewModel>()
     var textList = arrayListOf<String>()
     var checkList = arrayListOf<String>()
     val list_itemkey = mutableListOf<String>()
     private lateinit var key: String
-    private lateinit var idkey: ArrayList<String>
-    private var checkmarkIdList = mutableListOf<String>()
-    var count:Int =0
+
     @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -46,12 +42,11 @@ class TodoActivity : AppCompatActivity() {
         val listview = findViewById<ListView>(R.id.listView)
         calBtn = findViewById(R.id.CalBtn)
         toDoBtn = findViewById(R.id.TodoBtn)
-        clear = findViewById(R.id.clear)
-        delete = findViewById(R.id.delete)
+
         add = findViewById(R.id.add)
         val current = LocalDateTime.now()
         val formatter = DateTimeFormatter.ofPattern("yyyy년 MM월 dd일")
-        val dateTV = current.format(formatter).toString()
+        dateTV = current.format(formatter).toString()
 
         key = intent.getStringExtra("dateKey").toString()
 
@@ -63,66 +58,8 @@ class TodoActivity : AppCompatActivity() {
 
 
         listview.setOnItemClickListener { parent, view, position, id ->
-            var listposition = checkList[position]
-            var textposition = textList[position]
-
-            val postListener = object : ValueEventListener {
-
-                override fun onDataChange(dataSnapshot: DataSnapshot) {
-
-                    for (dataModel in dataSnapshot.children) {
-
-                        if(listposition.contains("true")){
-                            UserApiClient.instance.me { user, error ->
-                                FBRef.todoDate
-                                    .child(user!!.id.toString())
-                                    .child(dateTV.toString())
-                                    .child(list_itemkey[position])
-                                    .setValue(ListViewModel(textposition,click = "false"))
-                                checkList[position] = "false"
-
-                            }
-                            listviewAdapter.notifyDataSetChanged()
-                            break
-                        }
-
-                            //true 로 바꾸기
-                            if(listposition.contains("false")){
-                            UserApiClient.instance.me { user, error ->
-                                FBRef.todoDate
-                                    .child(user!!.id.toString())
-                                    .child(dateTV.toString())
-                                    .child(list_itemkey[position])
-                                   .setValue(ListViewModel(textposition,click = "true"))
-                                checkList[position] = "true"
-
-
-                            }
-                                listviewAdapter.notifyDataSetChanged()
-                                break
-                            }
-
-
-
-
-//true는 check로
-
-
-                    }
-                }
-                override fun onCancelled(databaseError: DatabaseError) {
-                }
-            }
-            UserApiClient.instance.me { user, error ->
-                FBRef.todoDate.child(user!!.id.toString())
-                    .child(dateTV)
-                    .addValueEventListener(postListener)
-            }
-//title이 같은애들일때 check로 바뀜
-
+          changeClick(position)
         }
-
-        clear.setOnClickListener { clearBtn()}
 
         //탭
         toDoBtn.setOnClickListener {
@@ -134,25 +71,72 @@ class TodoActivity : AppCompatActivity() {
         }
     }
 
+fun changeClick(position:Int){
+    var listposition = checkList[position]
+    var textposition = textList[position]
+    UserApiClient.instance.me { user, error ->
+        val postListener = object : ValueEventListener {
+
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+
+                for (dataModel in dataSnapshot.children) {
+
+                    if (listposition.contains("true")) {
+
+                        FBRef.todoDate
+                            .child(user!!.id.toString())
+                            .child(dateTV.toString())
+                            .child(list_itemkey[position])
+                            .setValue(ListViewModel(textposition, click = "false"))
+                            checkList[position] = "false"
+
+                        break
+
+                    }
+
+                    //true 로 바꾸기
+                    else if (listposition.contains("false")) {
+
+                        FBRef.todoDate
+                            .child(user!!.id.toString())
+                            .child(dateTV.toString())
+                            .child(list_itemkey[position])
+                            .setValue(ListViewModel(textposition, click = "true"))
+
+                        break
+                    }
+                }
+            }
 
 
+            override fun onCancelled(databaseError: DatabaseError) {
+            }
+        }
+        UserApiClient.instance.me { user, error ->
+            FBRef.todoDate.child(user!!.id.toString())
+                .child(dateTV)
+                .addValueEventListener(postListener)
+        }
+//title이 같은애들일때 check로 바뀜
 
-
-fun clearBtn(){
+    }
+    listviewAdapter.notifyDataSetChanged()
+}
+    fun clearBtn() {
 
         listviewAdapter.notifyDataSetChanged()
         UserApiClient.instance.me { user, error ->
-        FBRef.todoDate
-            .child(user!!.id.toString())
-            .child(key)
-            .removeValue()
+            FBRef.todoDate
+                .child(user!!.id.toString())
+                .child(key)
+                .removeValue()
 
-}
-    getCommentData(key)
-}
+        }
+        getCommentData(key)
+    }
 
-//오늘날짜 todolist에 가져오기
-    fun getCommentData(date:String) {
+    //오늘날짜 todolist에 가져오기
+    fun getCommentData(date: String) {
 //comment 아래 board아래 commentkey 아래 comment 데이터들
         val postListener = object : ValueEventListener {
             override fun onDataChange(dataSnapshot: DataSnapshot) {
@@ -163,17 +147,23 @@ fun clearBtn(){
 
                         for (i in dataModel.children) {
 
-                            list_item.add(ListViewModel(i.getValue(ListViewModel::class.java)!!.text.toString(),i.getValue(ListViewModel::class.java)!!.click.toString()) )
+                            list_item.add(
+                                ListViewModel(
+                                    i.getValue(ListViewModel::class.java)!!.text.toString(),
+                                    i.getValue(ListViewModel::class.java)!!.click.toString()
+                                )
+                            )
                             list_itemkey.add(i.key.toString())
                             textList.add(i.getValue(ListViewModel::class.java)!!.text.toString())
                             checkList.add(i.getValue(ListViewModel::class.java)!!.click.toString())
-                            Log.d("뭘가요",list_item.toString())
+                            Log.d("뭘가요", list_item.toString())
 
                         }
                     }
                     listviewAdapter.notifyDataSetChanged()//어댑터 동기화
                 }
             }
+
             override fun onCancelled(databaseError: DatabaseError) {
             }
         }
@@ -182,7 +172,6 @@ fun clearBtn(){
                 .addValueEventListener(postListener)
         }
     }
-
 
 
 }
